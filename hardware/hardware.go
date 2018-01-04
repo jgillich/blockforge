daf3374259665ecda818faa985a8cb44459b9e6a
Jakob Gillich <jakob@gillich.me>
2018-01-04 13:10:34 +0100

    fix accidential commit

--- hardware/hardware.go
+++ hardware/hardware.go
@@ -22,7 +22,6 @@ type GPU struct {
 }
 
 type CUDA struct {
-
 }
 
 func NewHardware() (*Hardware, error) {
@@ -35,47 +34,45 @@ func NewHardware() (*Hardware, error) {
 
 	//  n = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_OS_DEVICE);
 
-	num := h.GetNbobjsByType(hwloc.HWLOC_OBJ_OS_DEVICE)
+	num := h.GetNbobjsByType(hwloc.ObjectTypeOsDevice)
 
 	for i := 0; i < num; i++ {
-		o := h.GetObjByType(hwloc.HWLOC_OBJ_OS_DEVICE, i)
+		o := h.GetObjByType(hwloc.ObjectTypeOsDevice, i)
 
 		backend := o.InfoByName("Backend")
 
-		if backend  == "CUDA" {
+		if backend == "CUDA" {
 
 		} else if backend == "OpenCL" {
 
 		}
 
+		/*
+		           assert(!strncmp(obj->name, "cuda", 4));
+		           devid = atoi(obj->name + 4);
+		           printf("CUDA device %d\n", devid);
 
-/*
-        assert(!strncmp(obj->name, "cuda", 4));
-        devid = atoi(obj->name + 4);
-        printf("CUDA device %d\n", devid);
+		           s = hwloc_obj_get_info_by_name(obj, "GPUModel");
+		           if (s)
+		             printf("Model: %s\n", s);
 
-        s = hwloc_obj_get_info_by_name(obj, "GPUModel");
-        if (s)
-          printf("Model: %s\n", s);
+		           s = hwloc_obj_get_info_by_name(obj, "CUDAGlobalMemorySize");
+		           if (s)
+		             printf("Memory: %s\n", s);
 
-        s = hwloc_obj_get_info_by_name(obj, "CUDAGlobalMemorySize");
-        if (s)
-          printf("Memory: %s\n", s);
-
-        s = hwloc_obj_get_info_by_name(obj, "CUDAMultiProcessors");
-        if (s)
-        {
-          int mp = atoi(s);
-          s = hwloc_obj_get_info_by_name(obj, "CUDACoresPerMP");
-          if (s) {
-            int mp_cores = atoi(s);
-            printf("Cores: %d\n", mp * mp_cores);
-					}
-					*/
-}
-		}
-		//  obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_OS_DEVICE, i);
+		           s = hwloc_obj_get_info_by_name(obj, "CUDAMultiProcessors");
+		           if (s)
+		           {
+		             int mp = atoi(s);
+		             s = hwloc_obj_get_info_by_name(obj, "CUDACoresPerMP");
+		             if (s) {
+		               int mp_cores = atoi(s);
+		               printf("Cores: %d\n", mp * mp_cores);
+		   					}
+		*/
 	}
+
+	//  obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_OS_DEVICE, i);
 
 	return &hw, nil
 }
