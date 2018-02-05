d3259171dd35d654a85e35fccedbd4e30d70e9bf
Jakob Gillich <jakob@gillich.me>
2018-02-05 19:35:48 +0100

    windows build fix

--- hash/ethash/util.h
+++ hash/ethash/util.h
@@ -26,12 +26,6 @@
 extern "C" {
 #endif
 
-#ifdef _MSC_VER
-void debugf(char const* str, ...);
-#else
-#define debugf printf
-#endif
-
 static inline uint32_t min_u32(uint32_t a, uint32_t b)
 {
 	return a < b ? a : b;
